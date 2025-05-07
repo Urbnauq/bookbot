815d944b2a444c88e967d7b0e4ccc6135b23a880
urbnauq <eduardoalmonte18@gmai.com>
2025-05-07 12:32:06 -0400

    BookBot

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("greetings boots")
+import sys
+
+from stats import (
+    get_book_text,
+    count_book_words,
+    count_book_letters,
+    char_num_info,
+    report
+    )
+
+def main():
+    
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book = sys.argv[1]
+
+    book_contents = get_book_text(book)
+    total_words = count_book_words(book_contents)
+    total_letters = count_book_letters(book_contents)
+    char_nums = char_num_info(total_letters)
+    
+    report(total_words, char_nums, book)
+
+if __name__ == "__main__":
+    main()
---
+++ stats.py
@@ -0,0 +1,37 @@
+def get_book_text(book):
+    with open(book) as f:
+        book_contents = f.read()
+    return book_contents
+
+def count_book_words(book_contents):
+    nums_words = len(book_contents.split())
+    return nums_words
+
+def count_book_letters(book_contents):
+    dictionary = {}
+    for letter in book_contents:
+        dictionary[letter.lower()] = dictionary.get(letter.lower(), 0) + 1
+    return dictionary
+
+def char_num_info(dictionary):
+    char_nums = []
+    for key in dictionary:
+        if key.isalpha():
+            char_nums.append({"char": key, "num": dictionary[key]})
+    char_nums.sort(key=sort_on, reverse=True)
+    return char_nums
+
+# Sorting Method Key Function Helper
+def sort_on(dict):
+    return dict["num"]
+
+def report(total_words, char_nums, book):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    for letter_num in char_nums:
+        print(f"{letter_num['char']}: {letter_num['num']}")
+    print("============= END ===============")
+
